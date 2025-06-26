39b9b55e7fc550ceb5e140f3d45ba514e0cc69cb
Gianluigi Calcaterra <gianluigi.calcaterra@gmail.com>
2025-06-26 12:55:20 +0200

    Refactor NoTripCard; remove TopCardBoxDecoration and update layout for improved readability and consistency

--- lib/home/top_card/no_trip_card.dart
+++ lib/home/top_card/no_trip_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../../app_localizations.dart';
-import 'top_card_box_decoration.dart';
 
 class NoTripCard extends StatelessWidget {
   final AppLocalizations loc;
@@ -14,29 +13,30 @@ class NoTripCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TopCardBoxDecoration(
-      opacity: opacity,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              loc.get('no_trips_found'),
-              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                    fontWeight: FontWeight.w300, // light
-                    color: Colors.white, // testo bianco
-                  ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton.icon(
-              icon: const Icon(Icons.add),
-              label: Text(loc.get('add_trip')),
-              onPressed: onAddTrip,
-            ),
-          ],
+    return SizedBox.expand(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                loc.get('no_trips_found'),
+                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      fontWeight: FontWeight.w300,
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.add),
+                label: Text(loc.get('add_trip')),
+                onPressed: onAddTrip,
+              ),
+            ],
+          ),
         ),
       ),
     );
