89d8f850710859a0bc7cea6cade9f51bdd9c7602
Gianluigi Calcaterra <gianluigi.calcaterra@gmail.com>
2025-07-11 22:22:06 +0200

    fix: Add const constructor to AddNewExpensesGroupPage in EmptyGroupsState

--- lib/home/cards/widgets/empty_groups_state.dart
+++ lib/home/cards/widgets/empty_groups_state.dart
@@ -47,7 +47,7 @@ class EmptyGroupsState extends StatelessWidget {
             onPressed: () async {
               final result = await Navigator.of(context).push(
                 MaterialPageRoute(
-                  builder: (context) => AddNewExpensesGroupPage(),
+                  builder: (context) => const AddNewExpensesGroupPage(),
                 ),
               );
               if (result == true) {
