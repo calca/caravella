481f3ea5043a55770ce60ffbd658f3592a1ddf8d
Gianluigi Calcaterra <gianluigi.calcaterra@gmail.com>
2025-08-24 18:28:13 +0200

    Refactor DeveloperPage layout by adding SectionHeader widgets and modularizing rows for improved organization and readability

--- lib/settings/pages/developer_page.dart
+++ lib/settings/pages/developer_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:org_app_caravella/l10n/app_localizations.dart' as gen;
+import 'package:org_app_caravella/manager/group/widgets/section_header.dart';
 import 'package:org_app_caravella/settings/widgets/settings_card.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../widgets/app_toast.dart';
@@ -39,76 +40,106 @@ class DeveloperPage extends StatelessWidget {
     );
   }
 
+  Widget _buildGithubRow(BuildContext context, gen.AppLocalizations loc) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    return SettingsCard(
+      context: context,
+      color: colorScheme.surface,
+      child: ListTile(
+        leading: Icon(Icons.person_outline),
+        title: Text(loc.terms_github_title, style: textTheme.titleMedium),
+        subtitle: Text(loc.terms_github_desc, style: textTheme.bodySmall),
+        trailing: const Icon(Icons.launch, size: 16),
+        onTap: () => _launchUrl(context, 'https://github.com/calca'),
+      ),
+    );
+  }
+
+  Widget _buildRepoRow(BuildContext context, gen.AppLocalizations loc) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    return SettingsCard(
+      context: context,
+      color: colorScheme.surface,
+      child: ListTile(
+        leading: Icon(Icons.code_outlined),
+        title: Text(loc.terms_repo_title, style: textTheme.titleMedium),
+        subtitle: Text(loc.terms_repo_desc, style: textTheme.bodySmall),
+        trailing: const Icon(Icons.launch, size: 16),
+        onTap: () => _launchUrl(context, 'https://github.com/calca/caravella'),
+      ),
+    );
+  }
+
+  Widget _buildIssueRow(BuildContext context, gen.AppLocalizations loc) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    return SettingsCard(
+      context: context,
+      color: colorScheme.surface,
+      child: ListTile(
+        leading: Icon(Icons.bug_report_outlined),
+        title: Text(loc.terms_issue_title, style: textTheme.titleMedium),
+        subtitle: Text(loc.terms_issue_desc, style: textTheme.bodySmall),
+        trailing: const Icon(Icons.launch, size: 16),
+        onTap: () =>
+            _launchUrl(context, 'https://github.com/calca/caravella/issues'),
+      ),
+    );
+  }
+
+  Widget _buildLicenseRow(BuildContext context, gen.AppLocalizations loc) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    return SettingsCard(
+      context: context,
+      color: colorScheme.surface,
+      child: ListTile(
+        leading: Icon(Icons.open_in_new),
+        title: Text(loc.license_link, style: textTheme.titleMedium),
+        subtitle: Text(loc.terms_license_desc, style: textTheme.bodySmall),
+        trailing: const Icon(Icons.launch, size: 16),
+        onTap: () => _launchUrl(
+          context,
+          'https://github.com/calca/caravella/blob/main/LICENSE',
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final loc = gen.AppLocalizations.of(context);
-    final colorScheme = Theme.of(context).colorScheme;
-    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: colorScheme.surface,
-        foregroundColor: colorScheme.onSurface,
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        foregroundColor: Theme.of(context).colorScheme.onSurface,
         elevation: 0,
       ),
       body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
         children: [
+          SectionHeader(
+            title: "Developer",
+            description: "Information and links to support the developer",
+            padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+          ),
+          _buildGithubRow(context, loc),
           _buildBuyMeCoffeeRow(context, loc),
-          const SizedBox(height: 8),
-          SettingsCard(
-            context: context,
-            color: colorScheme.surface,
-            child: ListTile(
-              leading: Icon(Icons.person_outline),
-              title: Text(loc.terms_github_title, style: textTheme.titleMedium),
-              subtitle: Text(loc.terms_github_desc, style: textTheme.bodySmall),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 18),
-              onTap: () => _launchUrl(context, 'https://github.com/calca'),
-            ),
+          SectionHeader(
+            title: "Contribute",
+            description: "Information and links to support contributions",
+            padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
           ),
-          SettingsCard(
-            context: context,
-            color: colorScheme.surface,
-            child: ListTile(
-              leading: Icon(Icons.code_outlined),
-              title: Text(loc.terms_repo_title, style: textTheme.titleMedium),
-              subtitle: Text(loc.terms_repo_desc, style: textTheme.bodySmall),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 18),
-              onTap: () =>
-                  _launchUrl(context, 'https://github.com/calca/caravella'),
-            ),
+          _buildRepoRow(context, loc),
+          _buildIssueRow(context, loc),
+          SectionHeader(
+            title: "Other",
+            description: "Information and links to support other aspects",
+            padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
           ),
-          SettingsCard(
-            context: context,
-            color: colorScheme.surface,
-            child: ListTile(
-              leading: Icon(Icons.bug_report_outlined),
-              title: Text(loc.terms_issue_title, style: textTheme.titleMedium),
-              subtitle: Text(loc.terms_issue_desc, style: textTheme.bodySmall),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 18),
-              onTap: () => _launchUrl(
-                context,
-                'https://github.com/calca/caravella/issues',
-              ),
-            ),
-          ),
-          SettingsCard(
-            context: context,
-            color: colorScheme.surface,
-            child: ListTile(
-              leading: Icon(Icons.open_in_new),
-              title: Text(loc.license_link, style: textTheme.titleMedium),
-              subtitle: Text(
-                loc.terms_license_desc,
-                style: textTheme.bodySmall,
-              ),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 18),
-              onTap: () => _launchUrl(
-                context,
-                'https://github.com/calca/caravella/blob/main/LICENSE',
-              ),
-            ),
-          ),
+          _buildLicenseRow(context, loc),
         ],
       ),
     );
