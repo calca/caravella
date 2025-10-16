b80c7d43cd134c4e1f2f90970d4faada33af6f5a
Gianluigi Calcaterra <gianluigi.calcaterra@gmail.com>
2025-10-16 09:51:33 +0200

    Add Italian translations for user settings and update existing localizations in Spanish, Portuguese, and Chinese

--- lib/l10n/app_localizations_es.dart
+++ lib/l10n/app_localizations_es.dart
@@ -847,13 +847,13 @@ class AppLocalizationsEs extends AppLocalizations {
       'Habilitar el respaldo automático del sistema operativo';
 
   @override
-  String get settings_user_name_title => 'Your name';
+  String get settings_user_name_title => 'Tu nombre';
 
   @override
-  String get settings_user_name_desc => 'Name or nickname to use in the app';
+  String get settings_user_name_desc => 'Nombre o apodo para usar en la app';
 
   @override
-  String get settings_user_name_hint => 'Enter your name';
+  String get settings_user_name_hint => 'Ingresa tu nombre';
 
   @override
   String get info_tab => 'Info';
