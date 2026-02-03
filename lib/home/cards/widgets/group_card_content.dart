68b15c4f093358e42718a9f34875b0eb33020039
Gianluigi Calcaterra <gianluigi.calcaterra@gmail.com>
2026-02-03 09:03:17 +0100

    Refactor: Convert GroupCardContent to StatefulWidget for improved state management

--- lib/home/cards/widgets/group_card_content.dart
+++ lib/home/cards/widgets/group_card_content.dart
@@ -16,7 +16,7 @@ import 'group_card_recents.dart';
 /// - [GroupCardTotalAmount] - Total expenses display
 /// - [GroupCardStats] - Charts and statistics
 /// - [GroupCardAddButton] - Add expense button
-class GroupCardContent extends StatelessWidget {
+class GroupCardContent extends StatefulWidget {
   final ExpenseGroup group;
   final gen.AppLocalizations localizations;
   final ThemeData theme;
@@ -33,60 +33,101 @@ class GroupCardContent extends StatelessWidget {
   });
 
   @override
+  State<GroupCardContent> createState() => _GroupCardContentState();
+}
+
+class _GroupCardContentState extends State<GroupCardContent> {
+  late ExpenseGroup _currentGroup;
+  ExpenseGroupNotifier? _groupNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentGroup = widget.group;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // Listen to notifier for updates
+    _groupNotifier?.removeListener(_onGroupUpdated);
+    _groupNotifier = context.read<ExpenseGroupNotifier>();
+    _groupNotifier?.addListener(_onGroupUpdated);
+  }
+
+  @override
+  void dispose() {
+    _groupNotifier?.removeListener(_onGroupUpdated);
+    super.dispose();
+  }
+
+  void _onGroupUpdated() {
+    // Only update if this specific group was modified
+    if (_groupNotifier?.currentGroup?.id == widget.group.id) {
+      if (mounted) {
+        setState(() {
+          _currentGroup = _groupNotifier!.currentGroup!;
+        });
+      }
+    }
+  }
+
+  @override
+  void didUpdateWidget(GroupCardContent oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Update current group if the widget's group changed
+    if (oldWidget.group.id != widget.group.id) {
+      _currentGroup = widget.group;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Consumer<ExpenseGroupNotifier>(
-      builder: (context, groupNotifier, child) {
-        // If this group was updated, use data from the notifier
-        final currentGroup = (groupNotifier.currentGroup?.id == group.id)
-            ? groupNotifier.currentGroup!
-            : group;
+    final addExpenseController = AddExpenseController(
+      group: _currentGroup,
+      onExpenseAdded: widget.onExpenseAdded,
+      onCategoryAdded: widget.onCategoryAdded,
+    );
 
-        final addExpenseController = AddExpenseController(
-          group: currentGroup,
-          onExpenseAdded: onExpenseAdded,
-          onCategoryAdded: onCategoryAdded,
-        );
-
-        return LayoutBuilder(
-          builder: (ctx2, constraints) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GroupCardHeader(
-                  group: currentGroup,
-                  localizations: localizations,
-                  theme: theme,
-                ),
-                const Spacer(),
-                // Show playful empty state or amounts based on expenses
-                if (currentGroup.expenses.isEmpty)
-                  GroupCardEmptyState(
-                    localizations: localizations,
-                    theme: theme,
-                  )
-                else
-                  GroupCardAmounts(
-                    group: currentGroup,
-                    theme: theme,
-                    localizations: localizations,
-                  ),
-                const Spacer(),
-                GroupCardRecents(
-                  key: ValueKey(
-                    'recents_${currentGroup.id}_${currentGroup.expenses.length}',
-                  ),
-                  group: currentGroup,
-                  localizations: localizations,
-                  theme: theme,
-                ),
-                GroupCardAddButton(
-                  group: currentGroup,
-                  theme: theme,
-                  controller: addExpenseController,
-                ),
-              ],
-            );
-          },
+    return LayoutBuilder(
+      builder: (ctx2, constraints) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GroupCardHeader(
+              group: _currentGroup,
+              localizations: widget.localizations,
+              theme: widget.theme,
+            ),
+            const Spacer(),
+            // Show playful empty state or amounts based on expenses
+            if (_currentGroup.expenses.isEmpty)
+              GroupCardEmptyState(
+                localizations: widget.localizations,
+                theme: widget.theme,
+              )
+            else
+              GroupCardAmounts(
+                group: _currentGroup,
+                theme: widget.theme,
+                localizations: widget.localizations,
+              ),
+            const Spacer(),
+            GroupCardRecents(
+              key: ValueKey(
+                'recents_${_currentGroup.id}_${_currentGroup.expenses.length}',
+              ),
+              group: _currentGroup,
+              localizations: widget.localizations,
+              theme: widget.theme,
+            ),
+            GroupCardAddButton(
+              group: _currentGroup,
+              theme: widget.theme,
+              controller: addExpenseController,
+            ),
+          ],
         );
       },
     );
